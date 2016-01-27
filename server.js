063f033a6f8a25e94b6b09896f1bd0f08f6eba80
maxcarter <max4carter@gmail.com>
2016-01-26 23:33:21 -0500

    Start of Node.js server

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+var config = require('./config');
 var app = express();
+var port = config.port;
+
+mongoose.connect(config.database);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(port);
+console.log("Started Node.js server. Listening on port " + config.port);
